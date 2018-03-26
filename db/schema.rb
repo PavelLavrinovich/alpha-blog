d90e7e1c2142c34ad823393cacdd829fe8f472dc
Pavel Lavrinovich <1avrinovich.pav31@gmail.com>
2018-03-26 16:32:07 +0300

    add description

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180326132303) do
+ActiveRecord::Schema.define(version: 20180326133006) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
   end
 
 end
