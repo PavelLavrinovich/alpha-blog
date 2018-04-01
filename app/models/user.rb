1dacc028e619d62a05c84f4b8b9356ced2623c15
Pavel Lavrinovich <1avrinovich.pav31@gmail.com>
2018-04-01 15:57:57 +0300

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A.+@.+\.[a-z]+\z/i
 
+  validates :username, presence: true, length: { minimum: 3, maximum: 50 },
+            uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: { minimum: 3, maximum: 50 },
+            uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
 end
