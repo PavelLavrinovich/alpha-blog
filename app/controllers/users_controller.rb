7029b2cbcbf1a0a142346ce7570f7f59d015c4a4
Pavel Lavrinovich <1avrinovich.pav31@gmail.com>
2018-04-04 18:50:07 +0300

    Users listing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
+
   def new
     @user = User.new
   end
@@ -13,11 +15,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to edit_user_path(@user)
     else
@@ -26,10 +27,24 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def destroy
+    @user.destroy
+    flash[:notice] = 'The article has been deleted.'
+    redirect_to articles_path
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
