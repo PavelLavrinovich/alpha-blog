ff03ce6aa9e204a832492a3490e027d6be34a3da
Pavel Lavrinovich <1avrinovich.pav31@gmail.com>
2018-04-25 17:35:35 +0300

    Categories altering

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:edit, :show, :update, :destroy]
   before_action :require_admin, except: [:index, :show]
 
   def index
@@ -18,12 +19,34 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:notice] = 'The category has been updated.'
+      redirect_to category_path(@category)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category.destroy
+    flash[:notice] = 'The category has been deleted.'
+    redirect_to categories_path
+  end
+
   def show
-    @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
   def require_admin
     redirect_to log_in_path if !logged_in? || !current_user.admin?
